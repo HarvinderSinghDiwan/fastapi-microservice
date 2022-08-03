f825e8d15b440830931420d4ab8763640d4b6da1
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2022-08-03 13:09:12 +0530

    class product created

---
+++ inventory/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+redis = get_redis_connection(
+    host="redis-11844.c135.eu-central-1-1.ec2.cloud.redislabs.com",
+    port=11844,
+    password="pRdcpRkKPFn6UnEFskrDGxrmFbf5T9ER",
+    decode_responses=True
+)
+
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
+
+
