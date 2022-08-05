0be42d898db2b4a6a8fac8188baed73f5839c443
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2022-08-05 19:17:26 +0530

    product created  imported and maintained

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -1,6 +1,6 @@
 import {Products} from "./components/Products";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import {ProductsCreate} from "./components/ProductsCreate";
+
 import {Orders} from "./components/Orders";
 
 function App() {
