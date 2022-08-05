90edb66fa7ff6bf731847c6cf753854a2a6c70a5
Harvinder Singh Khalsa <harvindersinghdiwan@gmail.com>
2022-08-05 19:18:15 +0530

    Browser route added

--- inventory-frontend/src/App.js
+++ inventory-frontend/src/App.js
@@ -4,7 +4,13 @@ import {ProductsCreate} from "./components/ProductsCreate";
 import {Orders} from "./components/Orders";
 
 function App() {
-
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Products/>}/>
+            <Route path="/create" element={<ProductsCreate/>}/>
+            <Route path="/orders" element={<Orders/>}/>
+        </Routes>
+    </BrowserRouter>;
 }
 
 export default App;
